b8290eedfd7f5037a789f5b0424e85aaf7c5b2a2
maoMaocongcong <mhpan.tju@gmail.com>
2024-04-10 09:48:36 -0700

    connect front & back

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function index() {
   const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/home")
@@ -9,9 +10,22 @@ function index() {
       .then((data) => {
         // console.log(data);
         setMessage(data.message);
+        setPeople(data.people);
+        console.log(data.people);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }, []);
-  return <div>{message}</div>;
+  return (
+    <div>
+      <div>{message}</div>
+
+      {people.map((person, index) => (
+        <div key={index}>{person}</div>
+      ))}
+    </div>
+  );
 }
 
 export default index;
--- server.py
+++ server.py
@@ -7,7 +7,8 @@ CORS(app)
 @app.route("/api/home", methods=['GET'])
 def return_home():
 	return jsonify({
-			'message': "Hello world"
+			'message': "Hello world",
+			'people': ["Jerry", "Helen", "Happy"]
 		})
 
 
