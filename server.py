efa8c5de11902afc606b90a0efbb9709d0dfb333
maoMaocongcong <mhpan.tju@gmail.com>
2024-04-10 09:18:34 -0700

    2

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
-from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/api/home", methods=['GET'])
 def return_home():
 	return jsonify({
@@ -10,4 +12,4 @@ def return_home():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=8080)
